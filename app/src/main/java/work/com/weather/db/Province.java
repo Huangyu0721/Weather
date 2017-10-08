0b69943dfb28f40ac920539a2ec4fd5512891a0b
huangyu <hy960721@163.com>
2017-10-08 16:55:58 +0800

    创建数据库和表的各项配置

---
+++ app/src/main/java/work/com/weather/db/Province.java
@@ -0,0 +1,37 @@
+package work.com.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 10242 on 2017/10/8.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
